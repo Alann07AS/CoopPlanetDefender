8a2014432318f578ad83f6324d1a4f1bb8a8c7eb
Alann Schnebelen <122521922+Alann07AS@users.noreply.github.com>
2023-05-10 22:47:26 +0200

    Update planet.js whit new spritesheet

--- web/static/js/GO/planet.js
+++ web/static/js/GO/planet.js
@@ -7,7 +7,7 @@ const PlanetGameObjectInfo = new GOinfo
 PlanetGameObjectInfo.height = 200;
 PlanetGameObjectInfo.width = 200;
 
-PlanetGameObjectInfo.spritesAnimation.set("default", new SpriteAnimation("planet", "./static/assets/lavaplanet.png", 288, 288, 1, 0))
+PlanetGameObjectInfo.spritesAnimation.set("default", new SpriteAnimation("planet", "./static/assets/lavaplanetanim.png", 288, 288, 6, 1))
 
 PlanetGameObjectInfo.localRotatePoint.x = PlanetGameObjectInfo.localRotatePoint.y = PlanetGameObjectInfo.height/2
 
