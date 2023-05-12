be17a7a5342da70f8dcbae3f9a922f9e8bd99274
Alann <SchnebelenAlann@hotmail.com>
2023-05-12 16:20:11 +0200

    add sound hit planet

--- web/static/js/GO/planet.js
+++ web/static/js/GO/planet.js
@@ -17,13 +17,21 @@ PlanetGameObjectInfo.colision.circle.radius =
 PlanetGameObjectInfo.colision.circle.localX = 
 PlanetGameObjectInfo.colision.circle.localY = PlanetGameObjectInfo.height/2;
 
+const hitSoundBuffer = []
+const bufLen = 5
+for (let i=0;i<bufLen;i++) {hitSoundBuffer.push([new Audio("./static/sounds/sounds_hit.mp3"), false])}
+hitSoundBuffer.forEach((s, i)=>{
+    s[0].volume = 0.5
+    s[0].addEventListener('pause', ()=>{; s[1] = false})    
+})
 
 PlanetGameObjectInfo.updateHandler = (/**@type {GO}*/go) => {
     go.changeAngle(go.angleDeg+0.03)
     EnnemysPool.forEach(/**@param {GO} ene */(ene, index) => {
         if (go.colision.CheckCircle(ene)) {
             EnnemysPool.splice(index, 1);
-            lifebarGO.lifeValue -= ene.Domage
+            lifebarGO.lifeValue -= ene.Domage;
+            for (let i =0;i<bufLen;i++) {if (!hitSoundBuffer[i][1]) {hitSoundBuffer[i][0].play(); hitSoundBuffer[i][1] = true;break}}
             setTimeout(()=>ene.destroy(), 500)
         }
     });
